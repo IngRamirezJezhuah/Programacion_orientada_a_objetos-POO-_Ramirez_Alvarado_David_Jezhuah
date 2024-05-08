ea2c3c7cbdcb539814192f20350772b026dee8d9
IngRamirezJezhuah <150400667+IngRamirezJezhuah@users.noreply.github.com>
2024-05-07 19:56:55 -0600

    Add files via upload

---
+++ GItHub/Programacion Orientada a Objetos/act_4.py
@@ -0,0 +1,16 @@
+#c) raw_input(), print(area de un circulo)
+
+print("------------------")
+print(" ")
+print("Calcular el área de un triángulo")
+print(" ")
+print("------------------")
+
+base = float(input("Dame la base: "))
+altura = float(input("Dame la altura: "))
+
+area = base * altura / 2
+
+print("------------------")
+print("El área es:", area)
+print("------------------")
